1d643b4aecfb0732b639a08602bbdb216b08f0fb
Ben Hagan <benhagan@users.noreply.github.com>
2016-09-16 02:23:01 -0500

    Added pagination through ?offset query string

--- server.js
+++ server.js
@@ -29,27 +29,13 @@ MongoClient.connect(mongoURL, function(err, db) {
     fs.readFile('api.key', function(err, data) {
         var apiKey = data;
         var query = req.params.query;
-        var queryString = 'https://www.googleapis.com/customsearch/v1?q=' + query + '&cx=006762283887135438198%3Ay9tcn92rqp4&searchType=image&key=' + apiKey;
+        var offset = '';
+        if (req.query.offset != null) {
+          var offset = '&start=' + req.query.offset;
+        }
+        var queryString = 'https://www.googleapis.com/customsearch/v1?q=' + query + '&cx=006762283887135438198%3Ay9tcn92rqp4' + offset + '&searchType=image&key=' + apiKey;
 
-        // request(queryString, function(error, response, body) {
-        //   if (!error && response.statusCode == 200) {
-        //     var origRes = JSON.parse(body);
-        //     var newArr = origRes.items.map(function(currentValue, index, array) {
-        //       return {
-        //         url: currentValue.link,
-        //         snippet: currentValue.snippet,
-        //         thumbnail: currentValue.image.thumbnailLink,
-        //         title: currentValue.title,
-        //         context: currentValue.image.contextLink
-        //       };
-        //     });
-        //     res.json(newArr);
-        //   }
-        // });
-
-        //Mock out Google API for testing without hitting quota (100/day)
-        var response = {statusCode: 200};
-        fs.readFile('testAPI.json', 'utf8', function(error, body) {
+        request(queryString, function(error, response, body) {
           if (!error && response.statusCode == 200) {
             var origRes = JSON.parse(body);
             var newArr = origRes.items.map(function(currentValue, index, array) {
@@ -65,6 +51,25 @@ MongoClient.connect(mongoURL, function(err, db) {
             res.json(newArr);
           }
         });
+
+        //Mock out Google API for testing without hitting quota (100/day)
+        // var response = {statusCode: 200};
+        // fs.readFile('testAPI.json', 'utf8', function(error, body) {
+        //   if (!error && response.statusCode == 200) {
+        //     var origRes = JSON.parse(body);
+        //     var newArr = origRes.items.map(function(currentValue, index, array) {
+        //       return {
+        //         url: currentValue.link,
+        //         snippet: currentValue.snippet,
+        //         thumbnail: currentValue.image.thumbnailLink,
+        //         title: currentValue.title,
+        //         context: currentValue.image.contextLink
+        //       };
+        //     });
+        //     collection.insert({term: query, when: new Date().toISOString()});
+        //     res.json(newArr);
+        //   }
+        // });
     });
 
   });
